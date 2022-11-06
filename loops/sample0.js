b41a845f61c3d9a8401bb38dda23cd857e6d69ce
ginoongflores <f.christianpaul@outlook.com>
2022-11-06 21:56:34 +0800

    added loops sample

---
+++ loops/sample0.js
@@ -0,0 +1,14 @@
+// for loop
+// for (let i = 0; i < 100; i++) {
+
+// }
+
+
+// creating an array o strings
+let arr = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 1, 2, 3, 4, 5];
+
+for (let i = 0; i < arr.length; i++) {
+    if (typeof arr[i] === 'number') {
+        console.log(arr[i]);
+    }
+}
